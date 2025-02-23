55217a3ec47311603b42bd5bd2234060dea36280
devanshu-kr-jha <devanshu.kr.jha@gmail.com>
2025-02-23 20:51:19 +0530

    feat: add UserViewSet for managing user operations

--- django_rest_api/api/views.py
+++ django_rest_api/api/views.py
@@ -1,8 +1,50 @@
-from django.http import HttpResponse
+from rest_framework import viewsets, status
+from api.serializers import UserSerializer
+from api.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
-def test(request):
-    return HttpResponse("<h2>Hello world</h2>")
+class UserViewSet(viewsets.ViewSet):
+    parser_classes = [JSONParser]
 
+    def list(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
 
-# Create your views here.
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(
+            {"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT
+        )
